310bccf4d73e60e52ed613834cf8da7e2d12234e
MRX007 <mrx31995@gmail.com>
2023-12-25 18:39:41 +0500

    добавлен gh-pages

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 const path = require('path')
 module.exports = defineConfig({
+  publicPath: `/Vue_mini-chat/`,
   transpileDependencies: true,
   configureWebpack: {
     resolve: {
